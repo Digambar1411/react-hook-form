551e540c052e4838c1889f82d32fb6cef26ffdd0
digambar1411 <deshawaldigambar@gmail.com>
2025-05-18 23:09:41 +0530

    section class

--- src/components/ReactHookForm.tsx
+++ src/components/ReactHookForm.tsx
@@ -5,6 +5,10 @@ type SampleFormType = {
   username:string;
   email: string;
   gender:string;
+  address:{
+    street:string;
+    city:string;
+  }
 }
 
 const ReactHookForm = () => {
@@ -16,9 +20,9 @@ const ReactHookForm = () => {
     formState:{errors} 
   } = useForm<SampleFormType>({
     defaultValues:async () =>{
-      const data = await fetch('https://jsonplaceholder.typicode.com/users/1');
-      const json = await data.json();
-      return {username:json.username,email:json.email, gender:''}
+      const response = await fetch('https://jsonplaceholder.typicode.com/users/1');
+      const data = await response.json();
+      return {username:data.username,email:data.email, gender:'', address:{street:data.address.street,city:data.address.city}};
     }});
 
   const submitForm = (data: SampleFormType) => {
@@ -69,7 +73,18 @@ const ReactHookForm = () => {
           <label htmlFor="other">other</label>
           <input type="radio" id="other" value='other'  {...register('gender',{required:'gender is required'}) } />
         </div>
-        {errors.gender && <p className='error'>{errors.gender?.message}</p> }
+          {errors.gender && <p className='error'>{errors.gender?.message}</p> }
+
+        <div className='section'>
+          <label htmlFor="street">Street</label>
+          <input type="text" id='street' {...register('address.street',{required:'street address is required'})} />
+          {errors.address?.street && <p className='error'>{errors.address.street?.message}</p> }
+        </div>
+        <div className='section'>
+          <label htmlFor="city">City</label>
+          <input type="text" id='city' {...register('address.city',{required:'city is required'})}/>
+          {errors.address?.city && <p className='error'>{errors.address.city?.message}</p> }
+        </div>
       </div>
 
       <button type="submit">Submit</button>
