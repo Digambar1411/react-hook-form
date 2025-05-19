10ce61c0f66056b5d48e60a11f2b8c5d9b2f1fb9
digambar1411 <deshawaldigambar@gmail.com>
2025-05-19 13:29:07 +0530

    fix formatting

--- src/components/ReactHookForm.tsx
+++ src/components/ReactHookForm.tsx
@@ -1,5 +1,5 @@
 import { DevTool } from '@hookform/devtools';
-import {useFieldArray, useForm} from 'react-hook-form';
+import { useFieldArray, useForm } from "react-hook-form";
 
 type SampleFormType = {
   username:string;
@@ -42,97 +42,153 @@ const ReactHookForm = () => {
   }
 
   return (
-    <>
-    <form onSubmit={handleSubmit(submitForm)} noValidate>
-      <h1>Reack Hook Form</h1>
-      
-      <div className='section'>
-        <label htmlFor="username">Username</label>
-        <input type="text" placeholder='username' {...register('username',{required:'username is required'})}/>
-        { errors.username && <p className='error'>{errors.username?.message}</p> }
-      </div>
+		<>
+			<form onSubmit={handleSubmit(submitForm)} noValidate>
+				<h1>Reack Hook Form</h1>
 
-      <div className='section'>
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" {...register('email',
-        {
-          pattern:{
-            value:/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-            message:'Email is not valid'
-          },
-          validate:{ 
-            notAdmin:(value) => value !== 'admin@gmail.com' || ' This email is not allowed',
-            notBlockListedDomain:(value) => !value.endsWith('tcs.com') || 'This domain is not allowed'
-          },
-          required:'email is required'
-        })} />
-        {errors.email && <p className='error'>{errors.email?.message}</p> }
+				<div className="section">
+					<label htmlFor="username">Username</label>
+					<input
+						type="text"
+						placeholder="username"
+						{...register("username", { required: "username is required" })}
+					/>
+					{errors.username && (
+						<p className="error">{errors.username?.message}</p>
+					)}
+				</div>
 
-      </div>
+				<div className="section">
+					<label htmlFor="email">Email</label>
+					<input
+						type="email"
+						id="email"
+						{...register("email", {
+							pattern: {
+								value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+								message: "Email is not valid",
+							},
+							validate: {
+								notAdmin: (value) =>
+									value !== "admin@gmail.com" || " This email is not allowed",
+								notBlockListedDomain: (value) =>
+									!value.endsWith("tcs.com") || "This domain is not allowed",
+							},
+							required: "email is required",
+						})}
+					/>
+					{errors.email && <p className="error">{errors.email?.message}</p>}
+				</div>
 
-      <div className=''>
-        <label htmlFor="gender">Gender</label>
+				<div className="">
+					<label htmlFor="gender">Gender</label>
 
-        <div>
-          <label htmlFor="male">Male</label>
-          <input type="radio" id="male" value='male'  {...register('gender',{required:'gender is required'})}/>
-        </div>
-        <div>
-          <label htmlFor="female">Female</label>
-          <input type="radio" id="female" value='female'  {...register('gender',{required:'gender is required'})} />
-        </div>
-        <div>
-          <label htmlFor="other">other</label>
-          <input type="radio" id="other" value='other'  {...register('gender',{required:'gender is required'}) } />
-        </div>
-          {errors.gender && <p className='error'>{errors.gender?.message}</p> }
+					<div>
+						<label htmlFor="male">Male</label>
+						<input
+							type="radio"
+							id="male"
+							value="male"
+							{...register("gender", { required: "gender is required" })}
+						/>
+					</div>
+					<div>
+						<label htmlFor="female">Female</label>
+						<input
+							type="radio"
+							id="female"
+							value="female"
+							{...register("gender", { required: "gender is required" })}
+						/>
+					</div>
+					<div>
+						<label htmlFor="other">other</label>
+						<input
+							type="radio"
+							id="other"
+							value="other"
+							{...register("gender", { required: "gender is required" })}
+						/>
+					</div>
+					{errors.gender && <p className="error">{errors.gender?.message}</p>}
 
-          <div className='section'>
-            <label htmlFor="street">Street</label>
-            <input type="text" id='street' {...register('address.street',{required:'street address is required'})} />
-            {errors.address?.street && <p className='error'>{errors.address.street?.message}</p> }
-          </div>
-          <div className='section'>
-            <label htmlFor="city">City</label>
-            <input type="text" id='city' {...register('address.city',{required:'city is required'})}/>
-            {errors.address?.city && <p className='error'>{errors.address.city?.message}</p> }
-          </div>
-          <div className='section'>
-            <label htmlFor="primary-phone">Primary Phone</label>
-            <input type="number" id='phone1' {...register('phoneNumbers.0',{required:'primary phone is required'})}/>
-          </div>
+					<div className="section">
+						<label htmlFor="street">Street</label>
+						<input
+							type="text"
+							id="street"
+							{...register("address.street", {
+								required: "street address is required",
+							})}
+						/>
+						{errors.address?.street && (
+							<p className="error">{errors.address.street?.message}</p>
+						)}
+					</div>
+					<div className="section">
+						<label htmlFor="city">City</label>
+						<input
+							type="text"
+							id="city"
+							{...register("address.city", { required: "city is required" })}
+						/>
+						{errors.address?.city && (
+							<p className="error">{errors.address.city?.message}</p>
+						)}
+					</div>
+					<div className="section">
+						<label htmlFor="primary-phone">Primary Phone</label>
+						<input
+							type="number"
+							id="phone1"
+							{...register("phoneNumbers.0", {
+								required: "primary phone is required",
+							})}
+						/>
+					</div>
 
-          <div className='section'>
-            <label htmlFor="secondary-phone" >Secondary Phone</label>
-            <input type="number" id='phone2' {...register('phoneNumbers.1')}/>
-          </div>
+					<div className="section">
+						<label htmlFor="secondary-phone">Secondary Phone</label>
+						<input type="number" id="phone2" {...register("phoneNumbers.1")} />
+					</div>
 
-          <div className='section'>
-            <label htmlFor="phone-numbers">Phone Numbers</label>
-            <div>{
-              fields.map((field, index)=>{
-                return(
-                  <div key={field.id}>
-                    <input 
-                      type="text" 
-                      {...register(`phNumbers.${index}.number` as const)} 
-                    />
-                    {errors.phNumbers?.[index]?.number && <p className='error'>{errors.phNumbers[index].number?.message}</p> }
-                    {index > 0 && <button type='button' onClick={()=>remove(index)}> Remove </button>}
-                  </div>
-                )
-              })}
+					<div className="section">
+						<label htmlFor="phone-numbers">Phone Numbers</label>
+						<div>
+							{fields.map((field, index) => {
+								return (
+									<div key={field.id}>
+										<input
+											type="text"
+											{...register(`phNumbers.${index}.number` as const)}
+										/>
+										{errors.phNumbers?.[index]?.number && (
+											<p className="error">
+												{errors.phNumbers[index].number?.message}
+											</p>
+										)}
+										{index > 0 && (
+											<button type="button" onClick={() => remove(index)}>
+												{" "}
+												Remove{" "}
+											</button>
+										)}
+									</div>
+								);
+							})}
 
-              <button type='button' onClick={()=>append({number:''})}>Add More</button>
-            </div>
-          </div>
-        </div>
+							<button type="button" onClick={() => append({ number: "" })}>
+								Add More
+							</button>
+						</div>
+					</div>
+				</div>
 
-        <button type="submit">Submit</button>
-      </form>
-      <DevTool control={control} /> {/* Set up the dev tool */}
-    </>
-  )
+				<button type="submit">Submit</button>
+			</form>
+			<DevTool control={control} /> {/* Set up the dev tool */}
+		</>
+	);
 }
 
 export default ReactHookForm
