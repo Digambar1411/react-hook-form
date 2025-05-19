87d9571ca7f436da452c1a1f80a1ff8c016eb17b
digambar1411 <deshawaldigambar@gmail.com>
2025-05-19 14:57:07 +0530

    remove gender section

--- src/components/ReactHookForm.tsx
+++ src/components/ReactHookForm.tsx
@@ -53,10 +53,11 @@ const ReactHookForm = () => {
 					<input
 						type="text"
 						placeholder="username"
-						{...register("username", { 
-              validate: (value) => value !== "admin" || "This username is not allowed",
-              required: "username is required",
-            })}
+						{...register("username", {
+							validate: (value) =>
+								value !== "admin" || "This username is not allowed",
+							required: "username is required",
+						})}
 					/>
 					{errors.username && (
 						<p className="error">{errors.username?.message}</p>
@@ -78,7 +79,8 @@ const ReactHookForm = () => {
 									value !== "admin@gmail.com" || " This email is not allowed",
 								notBlockListedDomain: (value) =>
 									!value.endsWith("tcs.com") || "This domain is not allowed",
-                isSameOrg: (value) => value.endsWith('tcs-evms.com') || "Domain does not  match",
+								isSameOrg: (value) =>
+									value.endsWith("tcs-evms.com") || "Domain does not  match",
 							},
 							required: "email is required",
 						})}
@@ -86,107 +88,76 @@ const ReactHookForm = () => {
 					{errors.email && <p className="error">{errors.email?.message}</p>}
 				</div>
 
-				<div className="">
-					<label htmlFor="gender">Gender</label>
+				<div className="section">
+					<label htmlFor="street">Street</label>
+					<input
+						type="text"
+						id="street"
+						{...register("address.street", {
+							required: "street address is required",
+						})}
+					/>
+					{errors.address?.street && (
+						<p className="error">{errors.address.street?.message}</p>
+					)}
+				</div>
 
+				<div className="section">
+					<label htmlFor="city">City</label>
+					<input
+						type="text"
+						id="city"
+						{...register("address.city", { required: "city is required" })}
+					/>
+					{errors.address?.city && (
+						<p className="error">{errors.address.city?.message}</p>
+					)}
+				</div>
+        
+				<div className="section">
+					<label htmlFor="primary-phone">Primary Phone</label>
+					<input
+						type="number"
+						id="phone1"
+						{...register("phoneNumbers.0", {
+							required: "primary phone is required",
+						})}
+					/>
+				</div>
+
+				<div className="section">
+					<label htmlFor="secondary-phone">Secondary Phone</label>
+					<input type="number" id="phone2" {...register("phoneNumbers.1")} />
+				</div>
+
+				<div className="section">
+					<label htmlFor="phone-numbers">Other Phone Numbers</label>
 					<div>
-						<label htmlFor="male">Male</label>
-						<input
-							type="radio"
-							id="male"
-							value="male"
-							{...register("gender", { required: "gender is required" })}
-						/>
-					</div>
-					<div>
-						<label htmlFor="female">Female</label>
-						<input
-							type="radio"
-							id="female"
-							value="female"
-							{...register("gender", { required: "gender is required" })}
-						/>
-					</div>
-					<div>
-						<label htmlFor="other">other</label>
-						<input
-							type="radio"
-							id="other"
-							value="other"
-							{...register("gender", { required: "gender is required" })}
-						/>
-					</div>
-					{errors.gender && <p className="error">{errors.gender?.message}</p>}
+						{fields.map((field, index) => {
+							return (
+								<div key={field.id}>
+									<input
+										type="text"
+										{...register(`phNumbers.${index}.number` as const)}
+									/>
+									{errors.phNumbers?.[index]?.number && (
+										<p className="error">
+											{errors.phNumbers[index].number?.message}
+										</p>
+									)}
+									{index > 0 && (
+										<button type="button" onClick={() => remove(index)}>
+											{" "}
+											Remove{" "}
+										</button>
+									)}
+								</div>
+							);
+						})}
 
-					<div className="section">
-						<label htmlFor="street">Street</label>
-						<input
-							type="text"
-							id="street"
-							{...register("address.street", {
-								required: "street address is required",
-							})}
-						/>
-						{errors.address?.street && (
-							<p className="error">{errors.address.street?.message}</p>
-						)}
-					</div>
-					<div className="section">
-						<label htmlFor="city">City</label>
-						<input
-							type="text"
-							id="city"
-							{...register("address.city", { required: "city is required" })}
-						/>
-						{errors.address?.city && (
-							<p className="error">{errors.address.city?.message}</p>
-						)}
-					</div>
-					<div className="section">
-						<label htmlFor="primary-phone">Primary Phone</label>
-						<input
-							type="number"
-							id="phone1"
-							{...register("phoneNumbers.0", {
-								required: "primary phone is required",
-							})}
-						/>
-					</div>
-
-					<div className="section">
-						<label htmlFor="secondary-phone">Secondary Phone</label>
-						<input type="number" id="phone2" {...register("phoneNumbers.1")} />
-					</div>
-
-					<div className="section">
-						<label htmlFor="phone-numbers">Phone Numbers</label>
-						<div>
-							{fields.map((field, index) => {
-								return (
-									<div key={field.id}>
-										<input
-											type="text"
-											{...register(`phNumbers.${index}.number` as const)}
-										/>
-										{errors.phNumbers?.[index]?.number && (
-											<p className="error">
-												{errors.phNumbers[index].number?.message}
-											</p>
-										)}
-										{index > 0 && (
-											<button type="button" onClick={() => remove(index)}>
-												{" "}
-												Remove{" "}
-											</button>
-										)}
-									</div>
-								);
-							})}
-
-							<button type="button" onClick={() => append({ number: "" })}>
-								Add More
-							</button>
-						</div>
+						<button type="button" onClick={() => append({ number: "" })}>
+							Add More
+						</button>
 					</div>
 				</div>
 
