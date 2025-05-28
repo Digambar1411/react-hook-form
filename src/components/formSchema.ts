ddd934b62d55de38acbc845480e4cac72c50543d
digambar1411 <deshawaldigambar@gmail.com>
2025-05-28 18:39:01 +0530

    format schema

--- src/components/formSchema.ts
+++ src/components/formSchema.ts
@@ -1,17 +1,34 @@
 import { z } from 'zod';
 
 export const formSchema = z.object({
-  username:z.string({required_error:'username is required'}),
-  email:z.string({required_error:'email is required'}).email('email is not valid'),
-  address:z.object({
-    street:z.string({required_error:'street is required'}),
-    city:z.string({required_error:'city is required'}),
-  }),
-  phNumbers:z.array(z.object({number:z.string({required_error: 'number is required'})})),
-  age:z.number().min(1,'age is required').max(100,'age must be less than 100'),
-  dob:z.date(),
-  workTypes:z.array(z.enum(['part-time', 'full-time','hybrid'],{required_error:"work type is required"})).min(1,'At leas work type is required'),
-  resourceType:z.enum(['Contractor', 'Direct Hire'],{required_error:'Resource type is required'})
+  username: z
+    .string()
+    .nonempty('username cannot be empty')
+    .min(3, 'username must be at least 3 characters long')
+    .max(20, 'username must be less than 20 characters long'),
+  email: z
+    .string()
+    .nonempty('email is required')
+    .email('email is not valid'),
+  address: z
+    .object({
+      city: z
+        .string({ required_error: 'city is required' }),
+      street: z
+        .string({ required_error: 'street is required' }),
+    }),
+  phNumbers: z
+    .array(z
+      .object({ number: z.string({ required_error: 'number is required' }) })),
+  age: z
+    .number()
+    .min(1, 'age is required')
+    .max(100, 'age must be less than 100'),
+  dob: z.date(),
+  workTypes: z
+    .array(z
+      .enum(['part-time', 'full-time', 'hybrid'], { required_error: "work type is required" })),
+  resourceType: z.enum(['Contractor', 'Direct Hire'], { required_error: 'Resource type is required' })
 })
 
 
